eb191424d48565e547fb00742972a6d1e63bdc8d
杨雨翔 <1372201820@qq.com>
2022-08-05 15:53:27 +0800

    feat:PromiseA+规范的三种状态

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+//Promise/A+规范的三种状态
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+
 class MyPromise {
   // 构造方法接收一个回调
   constructor(executor) {
-    this._resolveQueue = []    // then收集的执行成功的回调队列
-    this._rejectQueue = []     // then收集的执行失败的回调队列
+    this._status = PENDING     // Promise状态
+    this._resolveQueue = []    // 成功队列, resolve时触发
+    this._rejectQueue = []     // 失败队列, reject时触发
 
     // 由于resolve/reject是在executor内部被调用, 因此需要使用箭头函数固定this指向, 否则找不到this._resolveQueue
     let _resolve = (val) => {
-      // 从成功队列里取出回调依次执行
+      if (this._status !== PENDING) return   // 对应规范中的"状态只能由pending到fulfilled或rejected"
+      this._status = FULFILLED              // 变更状态
+
+      // 这里之所以使用一个队列来储存回调,是为了实现规范要求的 "then 方法可以被同一个 promise 调用多次"
+      // 如果使用一个变量而非队列来储存回调,那么即使多次p1.then()也只会执行一次回调
       while (this._resolveQueue.length) {
         const callback = this._resolveQueue.shift()
         callback(val)
@@ -14,6 +24,8 @@ class MyPromise {
     }
     // 实现同resolve
     let _reject = (val) => {
+      if (this._status !== PENDING) return   // 对应规范中的"状态只能由pending到fulfilled或rejected"
+      this._status = REJECTED               // 变更状态
       while (this._rejectQueue.length) {
         const callback = this._rejectQueue.shift()
         callback(val)
@@ -23,16 +35,16 @@ class MyPromise {
     executor(_resolve, _reject)
   }
 
-  // then方法,接收一个成功的回调和一个失败的回调，并push进对应队列
+  // then方法,接收一个成功的回调和一个失败的回调
   then(resolveFn, rejectFn) {
     this._resolveQueue.push(resolveFn)
     this._rejectQueue.push(rejectFn)
   }
 }
 
-// const p1 = new MyPromise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve('result')
-//   }, 1000);
-// })
-// p1.then(res => console.log(res))
+const p1 = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('result')
+  }, 1000);
+})
+p1.then(res => console.log(res))
