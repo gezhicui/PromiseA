39a4bafda8d9a2f050a77aff8543b88914b97bdd
杨雨翔 <1372201820@qq.com>
2022-08-08 14:11:03 +0800

    feat:新增promise静态方法

--- index.js
+++ index.js
@@ -94,6 +94,93 @@ class MyPromise {
       }
     })
   }
+
+
+  // --------其余方法实现-------
+
+  //catch方法其实就是执行一下then的第二个回调
+  catch(rejectFn) {
+    return this.then(undefined, rejectFn)
+  }
+
+  /**
+   * finally()方法返回一个Promise。在promise结束时，无论结果是fulfilled或者是rejected，都会执行指定的回调函数。
+   * 在finally之后，还可以继续then。并且会将值原封不动的传递给后面的then
+   */
+
+  finally(callback) {
+    return this.then(
+      // MyPromise.resolve执行回调,并在then中return结果传递给后面的Promise,用于后续的链式调用
+      value => MyPromise.resolve(callback()).then(() => value),
+      reason => MyPromise.resolve(callback()).then(() => { throw reason })
+    )
+  }
+
+  /**
+   *  Promise.resolve(value)方法返回一个以给定值解析后的Promise 对象。如果该值为promise，返回这个promise；
+   * 如果这个值是thenable（即带有"then" 方法)），返回的promise会“跟随”这个thenable的对象，采用它的最终状态；
+   * 否则返回的promise将以此值完成。此函数将类promise对象的多层嵌套展平。
+   */
+
+  static resolve(value) {
+    if (value instanceof MyPromise) return value // 根据规范, 如果参数是Promise实例, 直接return这个实例
+    return new MyPromise(resolve => resolve(value))
+  }
+
+  //静态的reject方法
+  static reject(reason) {
+    return new MyPromise((resolve, reject) => reject(reason))
+  }
+
+  /**
+   * Promise.all(iterable)方法返回一个 Promise 实例，
+   * 此实例在 iterable 参数内所有的 promise 都“完成（resolved）”或参数中不包含 promise 时回调完成（resolve）；
+   * 如果参数中  promise 有一个失败（rejected），此实例回调失败（reject），失败原因的是第一个失败 promise 的结果。
+   */
+  static all(promiseArr) {
+    let index = 0
+    let result = []
+    return new MyPromise((resolve, reject) => {
+      promiseArr.forEach((p, i) => {
+        //Promise.resolve(p)用于处理传入值不为Promise的情况
+        MyPromise.resolve(p).then(
+          val => {
+            index++
+            result[i] = val
+            //所有then执行后, resolve结果
+            if (index === promiseArr.length) {
+              resolve(result)
+            }
+          },
+          err => {
+            //有一个Promise被reject时，MyPromise的状态变为reject
+            reject(err)
+          }
+        )
+      })
+    })
+  }
+
+  /**
+   * Promise.race(iterable)方法返回一个 promise，一旦迭代器中的某个promise解决或拒绝，返回的 promise就会解决或拒绝。
+   */
+  static race(promiseArr) {
+    return new MyPromise((resolve, reject) => {
+      //同时执行Promise,如果有一个Promise的状态发生改变,就变更新MyPromise的状态
+      for (let p of promiseArr) {
+        //Promise.resolve(p)用于处理传入值不为Promise的情况
+        MyPromise.resolve(p).then(
+          value => {
+            //注意这个resolve是上边new MyPromise的
+            resolve(value)
+          },
+          err => {
+            reject(err)
+          }
+        )
+      }
+    })
+  }
 }
 
 const p1 = new MyPromise((resolve, reject) => {
